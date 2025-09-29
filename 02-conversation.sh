2ad37121b63daaf6e7d09df5458f5fbaaf4439ab
Thrilok497 <devarpallithrilok@gmail.com>
2025-09-29 11:33:46 +0530

    practice

---
+++ 02-conversation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Thrilok: : Hi Hari, How are you ?"
+echo "Hari: : Hi Thrilok. I'm fine"
---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PERSON1="Thrilok"
+PERSON2="Hari"
+
+echo "$PERSON1: : Hi $PERSON2, How are you?"
+echo "$PERSON2: : Hi $PERSON1, I'm fine"
