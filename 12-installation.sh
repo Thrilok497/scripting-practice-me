3b204efa7298e3b77a57a41761acd9e4b9b38e25
Thrilok497 <devarpallithrilok@gmail.com>
2025-09-29 15:48:50 +0530

    practice

---
+++ 12-installation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: : Please run this script with root privelege"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: : Installing MySQL is failure"
+    exit 1
+else    
+    echo "Installing MySQL is SUCESS"
+fi 
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: : Installing nginx is failure"
+    exit 1
+else    
+    echo "Installing nginx is SUCESS"
+fi 
+
+dnf install mangodb-mongosh -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: : Installing mongodb-mongosh is failure"
+    exit 1
+else    
+    echo "Installing mongodb-mongosh is SUCESS"
+fi 
+
