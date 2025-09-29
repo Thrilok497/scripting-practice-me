9e391b0298b4178cd082660ac0450a99730b8167
Thrilok497 <devarpallithrilok@gmail.com>
2025-09-29 12:05:58 +0530

    practice

--- 07-variables.sh
+++ 07-variables.sh
@@ -3,6 +3,6 @@
 #DATE=$(date)
 START_TIME=$(date +%s)
 END_TIME=$(date +%s)
-TOTAL_TIME=$($END_TIME-$START_TIME)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 
 echo "Script executed in: $TOTAL_TIME seconds"
